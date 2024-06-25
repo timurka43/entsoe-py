e4560db53f7c5cf98437c9958b58badd5a26518a
Timur Kasimov <104742862+timurka43@users.noreply.github.com>
2024-06-25 13:33:37 +0100

    started work on the dataframe.py script

---
+++ dataframe.py
@@ -0,0 +1,12 @@
+
+import pandas as pd
+import mappings
+
+def pull_dataframe(country_codes, freq, start_year, end_year):
+    for country_code in country_codes:
+        country = mappings.COUNTRY_MAPPINGS[country_code]
+
+        
+
+
+    return
