e5c00788fbf2caaecc0b954494caa9ec2e6a2b3a
Timur Kasimov <104742862+timurka43@users.noreply.github.com>
2024-07-04 12:49:59 +0100

    added xmls folder

---
+++ mykey.py
@@ -0,0 +1,10 @@
+'''
+Name: mykey.py
+Author: Timur Kasimov
+Created: June 2024
+Updated: June 2024
+
+Purpose: My personal ENTSO-E key for accessing its API
+'''
+def get_key():
+    return '0a696472-3642-452e-ace4-96a440fdea99'
